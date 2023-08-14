9cc17ceb0fa3db16e1359f8ad6521e78288771e4
豊見山裕貴 <tomiyamayuki@yutakamiyamayukinoMacBook-Air.local>
2023-08-14 16:35:08 +0900

    4

---
+++ practice.py
@@ -0,0 +1,70 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+
+# MNISTデータセットをダウンロード
+mnist = fetch_openml('mnist_784', version=1, cache=True, as_frame=False)
+
+# 特徴量とラベルを取得
+X = mnist.data
+y = mnist.target
+
+# データを訓練用とテスト用に分割
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# モデルの構築と訓練
+model = MLPClassifier(hidden_layer_sizes=(128, 64), max_iter=1000, random_state=42)
+model.fit(X_train, y_train)
+
+# テストデータセットでの予測
+y_pred = model.predict(X_test)
+
+# 正答率の計算
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+# 手書き画像の読み込みと前処理
+i = 0
+while i <= 9:
+   image = Image.open("digits_"+str(i) + '.png').convert('L')  # 手書き画像のパスを指定して読み込む
+   image = image.resize((28, 28))  # 28x28にリサイズ
+   image = np.array(image)  # NumPy配列に変換
+   image = image.reshape(1, -1)  # 1次元の特徴量ベクトルに変換
+   #image = image / 255.0  # スケーリング (0から1の範囲に正規化)
+   image = 255.0 - image
+   # モデルによる予測
+   prediction = model.predict(image)
+   #testown = np.array([])
+   #test = np.append(testown,prediction[0])
+   print(prediction)
+   print(type(prediction))
+   i += 1
+
+
+   # 結果の表示
+   plt.imshow(image.reshape(28, 28), cmap='gray')
+   plt.title("number:"+str(i-1)+" Predicted Label: " + str(prediction[0]))
+   plt.axis('off')
+   plt.show()
+   
+i = 0
+while i <= 9:
+   image = Image.open("digits_"+str(i-1-1) + '.png').convert('L')  # 手書き画像のパスを指定して読み込む
+   image = image.resize((28, 28))  # 28x28にリサイズ
+   image = np.array(image)  # NumPy配列に変換
+   image = image.reshape(1, -1)  # 1次元の特徴量ベクトルに変換
+   image = 255.0 - image  # 色の反転
+   prediction = model.predict(image)  # モデルによる予測
+   print(prediction)
+   print(type(prediction))
+   i += 1
+
+   # 結果の表示
+   plt.imshow(image.reshape(28, 28), cmap='gray')
+   plt.title("number:"+str(i-1)+" Predicted Label: " + str(prediction[0]))
+   plt.axis('off')
+   plt.show()
